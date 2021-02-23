f3e8d12edc2cace2e156d8b109cbcaa4250b95a2
Marlon Balla <marlonballa07@gmail.com>
2021-02-23 00:10:01 -0300

    End of the first class

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
-import express from 'express';
-const app = express(); 
+import express, { response } from "express";
+const app = express();
+/* Get -> buscar,
+   Post -> salvar, 
+   Put -> Alterar, 
+   Delete -> deletar, 
+   Patch -> Alteração específica */
+
+   /* 1º Recurso
+      2º Request/Response */
+app.get("/", (request, response) => {
+    return response.send("Hello Word! NLW 04");
+})
+
+app.post("/", (request, response) => {
+    return response.json({message: "Dados salvos com sucesso :)"})
+})
 
 //cria o servidor (fica ouvindo a porta)
 app.listen(3333, () => console.log("Server is Running"));
