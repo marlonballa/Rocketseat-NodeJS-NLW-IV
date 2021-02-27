4aa4746607fc0c50d8f3446238d23a4879812f93
Marlon Balla <marlonballa07@gmail.com>
2021-02-26 22:38:55 -0300

    change controllers

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,10 @@
-import {Request, Response} from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User"
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UserRepository";
 class UserController {
     async create(request: Request, response: Response) {
         const { name, email} = request.body;
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UserRepository);
 
         //SELECT * FROM users WHERE email = "email"
         const userAlreadyExists = await usersRepository.findOne({
@@ -14,7 +14,7 @@ class UserController {
         if (userAlreadyExists) {
             return response.status(400).json({
                 error: "User already exists!"
-            })
+            });
         }
         const user = usersRepository.create({
             name, email
@@ -25,4 +25,4 @@ class UserController {
     }
 }
 
-export {UserController};
+export { UserController };
